acd87bd25ba612c1d733f4c772ba92246bf48f99
Y94411 <scottjason918@fullsail.edu>
2014-08-27 21:23:02 -0700

    didn't save..

--- SQLite/Resources/data.js
+++ SQLite/Resources/data.js
@@ -1,15 +1,36 @@
-var db = Ti.Database.install("/database/DBase.db", "db1");
+var db = Ti.Database.install("/database/DBase.db", "dTable");
 var data= [];
-var tableView = Ti.UI.createTableView({editable: true});
+var tabGroup = Ti.UI.createTabGroup();
+var tableView = Ti.UI.createTableView({});
 var create = function(){
-	var db = Ti.Database.open("db1");
-	db.execute("INSERT INTO db1 (name, nick, number) VALUES (?,?,?)", name, nick, number);
+	var db = Ti.Database.open("Dbase.db");
+	db.execute("INSERT INTO dTable (name, nick, number) VALUES (?,?,?)", name, nick, number);
 	db.close();
 };
 
 var read = function() {
-	var db = Ti.Database.open("db1");
-	var rows = db.execute("SELECT * FROM db1");
+ 	var win1 = Titanium.UI.createWindow({  
+	    title:'Contacts',
+	    backgroundColor:'#fff',
+	});
+	var tab1 = Titanium.UI.createTab({  
+    	icon:'KS_nav_views.png',
+    	title:'Contacts',
+	    window:win1
+ 	});
+
+	// var win2 = Titanium.UI.createWindow({  
+	    // title:'Add',
+	    // backgroundColor:'#fff'
+	// });
+	// var tab2 = Titanium.UI.createTab({  
+	    // icon:'KS_nav_ui.png',
+	    // title:'Add',
+	    // window:win2
+	// });
+
+	var db = Ti.Database.open("dTable");
+	var rows = db.execute("SELECT name, nick, number FROM dTable");
 	while(rows.isValidRow()){
 		var name = rows.fieldByName("name");
 		var nick = rows.fieldByName("nick");
@@ -22,50 +43,28 @@ var read = function() {
 		});
 		rows.next();
 	}
-	var tabGroup = Ti.UI.createTabGroup();
- 	var win1 = Titanium.UI.createWindow({  
-	    title:'Contacts',
-	    backgroundColor:'#fff',
-	});
-	var tab1 = Titanium.UI.createTab({  
-    	icon:'KS_nav_views.png',
-    	title:'Contacts',
-    	window: win1
- 	});
-	var win2 = Titanium.UI.createWindow({  
-	    title:'Add',
-	    backgroundColor:'#fff'
-	});
-	var tab2 = Titanium.UI.createTab({  
-	    icon:'KS_nav_ui.png',
-	    title:'Add',
-	    window:win2
-	});
-
+	console.log(db);
 	rows.close(); 
 	db.close();
-	console.log(data);
-	tableView.setData(data);
 	tabGroup.addTab(tab1);
-	tabGroup.addTab(tab2);
+	// tabGroup.addTab(tab2);
 	win1.add(tableView);
 	tabGroup.open();
 };
 
 var update = function(){
-	var dBase = Ti.Database.open("db1");
+	var dBase = Ti.Database.open("Dbase.sqlite");
 	dBase.execute("UPDATE dTable SET name=? nick=? number=? WHERE id=?", functions.nameText.value, functions.nickText.value, functions.numberText.value, functions.accept.id);
 	dBase.close();
 };
 
 var del = function(){
-	var dBase = Ti.Database.open("db1");
+	var dBase = Ti.Database.open("Dbase.sqlite");
 	dBase.execute("DELETE FROM dTable SET name=? nick=? number=? WHERE id=?");
 	dBase.close();
 };
 
-exports.del = del;
-exports.update = update;
+exports.tabGroup = tabGroup;
 exports.create = create;
 exports.read = read;
 exports.data = data;
